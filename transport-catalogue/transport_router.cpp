8bc0d933b602c1d6326c57b9a30bf7d69aa8e30e
konstantinbelousovEC <kb_sd@yahoo.com>
2023-01-22 13:17:15 +0700

    some corrections in TransportRouter class

--- transport-catalogue/transport_router.cpp
+++ transport-catalogue/transport_router.cpp
@@ -1,15 +1,53 @@
 #include "transport_router.h"
 
 namespace transport_router {
+    template<typename InputIterator>
+    void AddBusEdgesToGraph(TransportRouter& transport_router, InputIterator first, InputIterator last, std::string_view bus_name) {
+        for (; std::distance(first, last) != 1; first++) {
+            graph::VertexId from_id = transport_router.GetPairsOfVertices().at((*first)->name_).second;
+            const domain::Stop* from_stop = *first;
+            double time = 0.0;
+            InputIterator next_after_first = first;
+            for (std::advance(next_after_first, 1); next_after_first != last; next_after_first++) {
+                graph::VertexId to_id = transport_router.GetPairsOfVertices().at((*next_after_first)->name_).first;
+                time += transport_router.GetTransportCatalogue().GetDistancesBetweenStops(from_stop, *next_after_first) / METERS_PER_KM / transport_router.GetRoutingSettings().bus_velocity_ * MIN_PER_HOUR;
+                transport_router.GetGraph()->AddEdge({from_id, to_id, time});
+                from_stop = *next_after_first;
+                transport_router.GetEdgeDescription().push_back({
+                                                      EdgeType::BUS,
+                                                      bus_name,
+                                                      time,
+                                                      std::distance(first, next_after_first)
+                                              });
+            }
+        }
+    }
+
+    const RoutingSettings& TransportRouter::GetRoutingSettings() const & {
+        return routing_settings_;
+    }
+    const transport_catalogue::TransportCatalogue& TransportRouter::GetTransportCatalogue() const & {
+        return transport_catalogue_;
+    }
+    std::unique_ptr<Graph>& TransportRouter::GetGraph() & {
+        return graph_;
+    }
+    EdgeDescriptions& TransportRouter::GetEdgeDescription() & {
+        return edges_descriptions_;
+    }
+    std::unordered_map<std::string_view, std::pair<size_t, size_t>>& TransportRouter::GetPairsOfVertices() & {
+        return pairs_of_vertices_for_each_stop_;
+    }
+
     std::optional<EdgeDescriptions> TransportRouter::BuildRoute(std::string_view stop_from, std::string_view stop_to) const {
         EdgeDescriptions result;
 
         if (stop_from == stop_to) return result;
-        if (pairs_of_vertices_for_each_stop.count(stop_from) == 0
-            || pairs_of_vertices_for_each_stop.count(stop_to) == 0) return std::nullopt;
+        if (pairs_of_vertices_for_each_stop_.count(stop_from) == 0
+            || pairs_of_vertices_for_each_stop_.count(stop_to) == 0) return std::nullopt;
 
-        graph::VertexId from_id = pairs_of_vertices_for_each_stop.at(stop_from).first;
-        graph::VertexId to = pairs_of_vertices_for_each_stop.at(stop_to).first;
+        graph::VertexId from_id = pairs_of_vertices_for_each_stop_.at(stop_from).first;
+        graph::VertexId to = pairs_of_vertices_for_each_stop_.at(stop_to).first;
         std::optional<Router::RouteInfo> route = router_->BuildRoute(from_id, to);
 
         if (!route.has_value()) return std::nullopt;
@@ -23,9 +61,9 @@ namespace transport_router {
     void TransportRouter::FillGraph() {
         AddWaitEdgesToGraph();
         for (auto [name, bus_ptr] : transport_catalogue_.GetBusIndexes()) {
-            AddBusEdgesToGraph(bus_ptr->stops_.begin(), bus_ptr->stops_.end(), name);
+            AddBusEdgesToGraph(*this, bus_ptr->stops_.begin(), bus_ptr->stops_.end(), name);
             if (bus_ptr->type_ == domain::BusType::REVERSE) {
-                AddBusEdgesToGraph(bus_ptr->stops_.rbegin(), bus_ptr->stops_.rend(), name);
+                AddBusEdgesToGraph(*this, bus_ptr->stops_.crbegin(), bus_ptr->stops_.crend(), name);
             }
         }
     }
@@ -35,7 +73,7 @@ namespace transport_router {
         graph::VertexId to_id = 1;
         for (std::string_view name: transport_catalogue_.GetUsedStopNames()) {
             graph_->AddEdge({from_id, to_id, routing_settings_.bus_wait_time_});
-            pairs_of_vertices_for_each_stop.insert({name, {from_id, to_id}});
+            pairs_of_vertices_for_each_stop_.insert({name, {from_id, to_id}});
             edges_descriptions_.push_back({
                 EdgeType::WAIT,
                 name,
--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -6,6 +6,9 @@
 #include <memory>
 
 namespace transport_router {
+    constexpr static double METERS_PER_KM = 1000.0;
+    constexpr static double MIN_PER_HOUR = 60.0;
+
     struct RoutingSettings {
         double bus_wait_time_ = 0.0;
         double bus_velocity_ = 0.0;
@@ -39,6 +42,12 @@ namespace transport_router {
             router_ = std::make_unique<Router>(*graph_);
         }
 
+        const RoutingSettings& GetRoutingSettings() const &;
+        const transport_catalogue::TransportCatalogue& GetTransportCatalogue() const &;
+        std::unique_ptr<Graph>& GetGraph() &;
+        EdgeDescriptions& GetEdgeDescription() &;
+        std::unordered_map<std::string_view, std::pair<size_t, size_t>>& GetPairsOfVertices() &;
+
         std::optional<EdgeDescriptions> BuildRoute(std::string_view stop_from, std::string_view stop_to) const;
 
     private:
@@ -46,38 +55,10 @@ namespace transport_router {
         const transport_catalogue::TransportCatalogue& transport_catalogue_;
         std::unique_ptr<Graph> graph_;
         std::unique_ptr<Router> router_;
-        std::unordered_map<std::string_view, std::pair<size_t, size_t>> pairs_of_vertices_for_each_stop;
+        std::unordered_map<std::string_view, std::pair<size_t, size_t>> pairs_of_vertices_for_each_stop_;
         EdgeDescriptions edges_descriptions_;
-
-        constexpr static const double METERS_PER_KM = 1000.0;
-        constexpr static const double MIN_PER_HOUR = 60.0;
 
         void FillGraph();
         void AddWaitEdgesToGraph();
-
-        template<typename InputIterator>
-        void AddBusEdgesToGraph(InputIterator first, InputIterator last, std::string_view bus_name);
     };
-
-    template<typename InputIterator>
-    void TransportRouter::AddBusEdgesToGraph(InputIterator first, InputIterator last, std::string_view bus_name) {
-        for (; std::distance(first, last) != 1; first++) {
-            graph::VertexId from_id = pairs_of_vertices_for_each_stop.at((*first)->name_).second;
-            const domain::Stop* from_stop = *first;
-            double time = 0.0;
-            InputIterator next_after_first = first;
-            for (std::advance(next_after_first, 1); next_after_first != last; next_after_first++) {
-                graph::VertexId to_id = pairs_of_vertices_for_each_stop.at((*next_after_first)->name_).first;
-                time += transport_catalogue_.GetDistancesBetweenStops(from_stop, *next_after_first) / METERS_PER_KM / routing_settings_.bus_velocity_ * MIN_PER_HOUR;
-                graph_->AddEdge({from_id, to_id, time});
-                from_stop = *next_after_first;
-                edges_descriptions_.push_back({
-                        EdgeType::BUS,
-                        bus_name,
-                        time,
-                        std::distance(first, next_after_first)
-                });
-            }
-        }
-    }
 }
